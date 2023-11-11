845bd38cc31801f48d55428f68dc01bb79cfbe4b
Theo Kenzo de Araújo <theokenzo35@gmail.com>
2023-11-11 19:42:40 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <header className='flex justify-center items-center w-full flex-col p-2 bg-slate-300'>
-        <h1>Perdi</h1>
+        <h1>JSQuiz</h1>
         <h1>Ana Cristina Thiersch da Cruz, Aliny Alves Dutra, Iago Clemilson Silva Santos e Theo Kenzo de Araújo</h1>
         <h1>11/11/2023</h1>
       </header>
